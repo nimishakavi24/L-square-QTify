27691b3c3f44e058ec9761225a48f3e612fd3edd
nimishakavisriram <nimishakavisriram24Gmail.com>
2024-02-01 19:04:24 +0530

    implement the card

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,13 +1,47 @@
-/* eslint-disable no-unused-vars */
-import Navbar from "../src/components/Navbar/Navbar";
-import Hero from "../src/components/Hero/Hero";
-
-
+// import styles from './App.module.css';
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import Hero from './components/Hero/Hero';
+import { useState , useEffect } from 'react';
+import { fetchTopAlbums ,fetchNewAlbums} from './api/api';
+import Section from './components/Section/Section';
+import  styles from './App.module.css';
 function App() {
+  const [topAlbumData,setTopAlbumData]=useState([]);
+  const [newAlbumData,setNewAlbumData] = useState([]);
+
+  const genrateTopAlbum=async () => {
+    try{
+      const data = await fetchTopAlbums();
+      setTopAlbumData(data);
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  const genrateNewAlbum=async () => {
+    try{
+      const data = await fetchNewAlbums();
+      setNewAlbumData(data);
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    genrateTopAlbum();
+    genrateNewAlbum();
+  },[]);
   return (
-    <div>
-      <Navbar />
-      <Hero />
+    <div >
+      <Navbar/>
+      <Hero/>
+      <div className={styles.sectionWrapper}>
+      <Section data = {topAlbumData} title="Top Albums" type="album"/>
+      <Section data = {newAlbumData} title="New Albums" type="album"/>
+      </div>
+      {/* <AlbumGrid data={topAlbumData} title="Top Album"/>
+      <AlbumGrid data={newAlbumData} title="New Album"/> */}
     </div>
   );
 }
---
+++ qtify/src/api/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+export const BACKEND_ENDPOINT ='https://qtify-backend-labs.crio.do';
+export const fetchTopAlbums =async()=>{
+    try{
+        const res=await axios.get(`${BACKEND_ENDPOINT}/albums/top`);
+        return res.data;
+    }catch(err){
+        console.error(err);
+    }
+}
+export const fetchNewAlbums=async ()=>{
+    try{
+    const res = await axios.get(`${BACKEND_ENDPOINT}/albums/new`);
+    console.log(res);
+    return res.data;
+}
+catch(err){
+    console.log(err);
+}
+
+}
