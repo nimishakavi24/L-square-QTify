ad57ee40ac22f3fbd7b6183089d37c6a54bf98f2
nimishakavisriram <nimishakavisriram24Gmail.com>
2024-02-01 10:32:08 +0530

    Hero

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import Navbar from "../src/components/Navbar/Navbar";
+import Hero from "../src/components/Hero/Hero";
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ qtify/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HeroImage from '../assets/vibrating-headphone 1.png';
+import styles from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+        <div>
+            <h1>100 Thousand Songs, ad-free</h1>
+            <h1>Over thousands podcast episodes</h1>
+        </div>
+        <div>
+            <img src={HeroImage} width={212}alt='headphones'/>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
